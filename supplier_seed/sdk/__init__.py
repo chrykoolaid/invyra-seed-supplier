d453af7fe6cd9bad347efd31ee5ac2940364f1dc
chrykoolaid <atillacokyavuz1992@gmail.com>
2026-07-15 23:07:20 +0800

    feat(sdk): export async read client

--- supplier_seed/sdk/__init__.py
+++ supplier_seed/sdk/__init__.py
@@ -1,3 +1,8 @@
+from supplier_seed.sdk.async_client import SupplierSeedAsyncReadClient
 from supplier_seed.sdk.client import SupplierSeedApiError, SupplierSeedReadClient
 
-__all__ = ["SupplierSeedApiError", "SupplierSeedReadClient"]
+__all__ = [
+    "SupplierSeedApiError",
+    "SupplierSeedAsyncReadClient",
+    "SupplierSeedReadClient",
+]
