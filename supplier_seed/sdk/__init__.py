4963ef0f8e1f112046a13ce7658f39ee9d28d252
chrykoolaid <atillacokyavuz1992@gmail.com>
2026-07-15 23:25:34 +0800

    feat(sdk): export typed enterprise resource models

--- supplier_seed/sdk/__init__.py
+++ supplier_seed/sdk/__init__.py
@@ -1,8 +1,18 @@
 from supplier_seed.sdk.async_client import SupplierSeedAsyncReadClient
 from supplier_seed.sdk.client import SupplierSeedApiError, SupplierSeedReadClient
+from supplier_seed.sdk.models import (
+    AuditEventResource,
+    QueueResource,
+    SupplierDetailResource,
+    SupplierSummaryResource,
+)
 
 __all__ = [
+    "AuditEventResource",
+    "QueueResource",
+    "SupplierDetailResource",
     "SupplierSeedApiError",
     "SupplierSeedAsyncReadClient",
     "SupplierSeedReadClient",
+    "SupplierSummaryResource",
 ]
